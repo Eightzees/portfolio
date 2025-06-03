d26b72f0a700765bf186c652455a2a37182d0330
Eightzees <nuffgroove0@gmail.com>
2025-06-03 18:40:03 +0900

    edited vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "portfolio" : "./",
+  base: process.env.GITHUB_PAGES ? "/portfolio/" : "./",
   plugins: [react()],
 });
